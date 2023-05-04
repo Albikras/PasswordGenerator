dc59a4015c3f50467027d5cdd6768fafcb999816
Albikras <124819029+Albikras@users.noreply.github.com>
2023-05-04 16:54:34 -0400

    Finshed html, Javascript, and css portions

--- script.js
+++ script.js
@@ -1,205 +1,72 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var numbers = document.getElementById("numbers");
+var lowerCaseLetters = document.getElementById("lowerCaseLetters");
+var upperCaseLetters = document.getElementById("upperCaseLetters");
+var specialCharacters = document.getElementById("specialCharacters");
+var passwordLength = document.getElementById("passwordLength");
+
+numbers.checked = true;
+lowerCaseLetters.checked = true;
+upperCaseLetters.checked = true;
+specialCharacters.checked = true;
+
+
+var arrayNumbers = ["0","1","2","3","4","5","6","7","8","9"];
+var arrayLowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var arrayUpperCaseLetters = ["A","B","C","D",'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var arraySpecialCharacters = ["!","@","#","$","%","^","&","*","(",")","?","/","<",">",",",".",":",";"];
 // Write password to the #password input
-var generateBtn = document.querySelector("#generate");
-var generateBtn = document.querySelector("#generate");
-
-
-var pass="";
-
-var numbers =["0","1","2","3","4","5","6","7","8","9"];
-var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var capital = ["A","B","C","D",'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var special = ["!","@","#","$","%","^","&","*","(",")","?","/","<",">",",",".",":",";"];
 
 function writePassword() {
-  var password = generatePassword();
-  
-  var passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
-  
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
 }
 
 function generatePassword(){
-  
-  var lower = prompt("Do you want lower case letters (Y or N)");
-  var upper = prompt("Do you want upper case letters (Y or N)");
-  var num = prompt("Do you want numbers (Y or N)");
-  var spec = prompt("Do you want special characters (Y or N)");
-  var amount = prompt("between 8-128 how long do you want the password");
-  var empty = [];
-  if(lower == "Y" && upper == "N" && num == "N" && spec == "N"){
-      empty = characters;
-    }else if(lower == "N" && upper == "Y" && num == "N" && spec == "N"){
-      empty = capital;
-  
-    }else if(lower == "N" && upper == "N" && num == "Y" && spec == "N"){
-      empty = numbers;
-  
-    }else if(lower == "N" && upper == "N" && num == "N" && spec == "Y"){
-      empty = special;
-  
-    }else if(lower == "Y" && upper == "Y" && num == "N" && spec == "N"){
-      empty = characters.concat(capital);
-   
-    }else if(lower == "Y" && upper == "N" && num == "Y" && spec == "N"){
-      empty = characters.concat(numbers);
-  
-    }else if(lower == "Y" && upper == "N" && num == "N" && spec == "Y"){
-      empty = characters.concat(special);
-  
-    }else if(lower == "N" && upper == "Y" && num == "Y" && spec == "N"){
-      empty = capital.concat(numbers);
-   
-    }else if(lower == "N" && upper == "Y" && num == "N" && spec == "Y"){
-      empty = capital.concat(special);
-   
-    }else if(lower == "N" && upper == "N" && num == "Y" && spec == "Y"){
-      empty = numbers.concat(special);
-   
-    }else if(lower == "Y" && upper == "Y" && num == "Y" && spec == "N"){
-      empty = lower.concat(numbers, capital);
-   
-    }else if(lower == "Y" && upper == "N" && num == "Y" && spec == "Y"){
-      empty = capital.concat(numbers);
+    var emptyArray = [];
+    var pass=""
+    if(numbers.checked === false && lowerCaseLetters.checked === false &&
+    specialCharacters.checked === false && upperCaseLetters.checked === false){
+        
+        alert("invalid Input Please Chack One Of The Boxes!!!");
+        return pass;
     }
-    else if(lower == "Y" && upper == "Y" && num == "Y" && spec == "N"){
-      empty = characters.concat(capital, numbers);
+    if(passwordLength.value<8 || passwordLength.value>128){
+        alert("Invalid Input");
+        return pass;
     }
-    else if(lower == "Y" && upper == "N" && num == "Y" && spec == "Y"){
-      empty = characters.concat(special, numbers);
+    if(numbers.checked === true){
+        emptyArray = emptyArray.concat(arrayNumbers);
+        console.log(numbers.value);
     }
-    else if(lower == "Y" && upper == "Y" && num == "N" && spec == "Y"){
-      empty = characters.concat(capital, special);
+    if(lowerCaseLetters.checked === true){
+        emptyArray = emptyArray.concat(arrayLowerCaseLetters);
+        console.log(lowerCaseLetters.value);
     }
-    else if(lower == "N" && upper == "Y" && num == "Y" && spec == "Y"){
-      empty = capital.concat(numbers, special);
+    if(upperCaseLetters.checked === true){
+        emptyArray = emptyArray.concat(arrayUpperCaseLetters);
+        console.log(upperCaseLetters.value);
     }
-    else if(lower == "Y" && upper == "Y" && num == "Y" && spec== "Y"){
-      empty = characters.concat(capital, special, numbers);
+    if(specialCharacters.checked === true){
+        emptyArray = emptyArray.concat(arraySpecialCharacters);
+        console.log(specialCharacters.value);
     }
-    for(var i = 0; i<amount; i++){
-    var min = 0;
-    var max = empty.length;
-    var random = Math.floor(Math.random() * (max+ min+1)- min);
-    pass = pass + empty[random]; 
-    }
-    return pass;
-  }
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+    for(var i = 0; i<passwordLength.value; i++){
+        var min = 0;
+        var max = emptyArray.length;
+        var random = Math.floor(Math.random() * emptyArray.length);
+        pass = pass + emptyArray[random]; 
+        console.log(pass);
+        }
+        console.log(pass);
+        return pass;
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  //   var amount = prompt("between 8-128 how long do you want the password");
-  // }else if(lower == "Y" && upper == "N" && number == "N" && special == "N"){
-  //   for(var i = 0; i<amount; i++){
-    //   var min = 0;
- //   var max = 25;
- //   var random = Math.floor(Math.random() * (max+ min+1)- min);
- //   pass = pass +characters[random];
- //  }
- // }else if(lower == "N" && upper == "N" && number == "N" && special == "Y"){
- //   var amount = prompt("between 8-128 how long do you want the password");
- //   for(var i = 0; i<amount; i++){
- //   var min = 0;
- //   var max = 17;
- //   var random = Math.floor(Math.random() * (max+ min+1)- min);
- //   pass = pass +special[random];
- //  }
- // }else if(lower == "N" && upper == "Y" && number == "N" && special == "N"){
- //   var amount = prompt("between 8-128 how long do you want the password");
- //   for(var i = 0; i<amount; i++){
- //   var min = 0;
- //   var max = 25;
- //   var random = Math.floor(Math.random() * (max+ min+1)- min);
- //   pass = pass +capital[random];
- //  }
- // }else if(lower == "Y" && upper == "N" && number == "Y" && special == "N"){
- //   var amount = prompt("between 8-128 how long do you want the password");
- //   for(var i = 0; i<amount; i++){
- //   var min = 0;
- //   var max = 34;
- //   var random = Math.floor(Math.random() * (max+ min+1)- min);
- //   pass = pass +numswlower[random];
- //  }
- // }else if(lower == "Y" && upper == "Y" && number == "N" && special == "N"){
- //   var amount = prompt("between 8-128 how long do you want the password");
- //   for(var i = 0; i<amount; i++){
- //   var min = 0;
- //   var max = 34;
- //   var random = Math.floor(Math.random() * (max+ min+1)- min);
- //   pass = pass +upperlower[random];
- //  }
- // }else if(lower == "Y" && upper == "Y" && number == "N" && special == "N"){
- //   var amount = prompt("between 8-128 how long do you want the password");
- //   for(var i = 0; i<amount; i++){
- //   var min = 0;
- //   var max = 34;
- //   var random = Math.floor(Math.random() * (max+ min+1)- min);
- //   pass = pass +numspec[random];
- //  }
- // }
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
